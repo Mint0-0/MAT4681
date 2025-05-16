6cc32013d5d40aa47c5b06c8bab5311ee15b6f10
MINT <155094852+Mint0-0@users.noreply.github.com>
2025-05-16 11:42:29 -0400

    ajout de l'exercice 3 dans Chapitre_1.R

--- Chapitre_1.R
+++ Chapitre_1.R
@@ -1,6 +1,61 @@
 ############################# CHAPITRE 1 #######################################
 
+# numero 3
 
+revenus <- c(4.9,2.4,9.8,3.8,7.7,6.0,3.3,3.6,4.7,6.9,5.2,2.6,2.9,4.8,9.0,4.3,
+             1.6,2.6,0.8,4.1, 4.8,4.4,4.5,3.6,8.2,2.4,3.3,10.3,4.4,5.3,11.6,
+             7.7,4.6,5.6,3.7,5.2,6.4,2.4,0.6,4.6,6.9,0.1,3.5,1.0,3.1,8.2,2.9,
+             6.7,4.5,4.4,5.3,5.7,2.3,4.6,1.4,1.8,5.9,6.5,5.1,6.8,7.8,7.6,7.7,
+             10.8,4.8,2.4,2.0,3.2,4.1,4.5,3.5,3.9,7.9,2.0,5.5,4.8,5.9,1.3,3.9,
+             7.9,0.8,7.4,9.9,3.4,4.4,3.2,11.1,3.6,5.6,2.0,8.2,4.9,4.3,3.3,3.0,
+             5.0,0.3,7.7,4.9,6.2,3.2,4.7,7.9,5.5,8.8,5.7,2.3,3.5,1.5,10.9,4.1,
+             4.2,4.7,0.7,3.5,2.8,4.4,5.9,6.0,6.8,8.1,4.1,8.0,2.8,9.4,5.2,5.4,
+             5.4,0.6,3.9,8.4,2.0,6.7,3.8,1.8,8.3,2.8,2.8,10.3,0.6,3.4,3.7,3.8,
+             4.3,6.5,1.6,8.3,10.4,5.6,4.6)
+
+# histogramme
+hist_data <- hist(revenus,
+                # breaks = 12,
+                  main = "Histogramme du salaire", 
+                  xlab = "Pourcentage de ventes",
+                  ylab = "Effectif",
+                  col = "pink",
+                  right = FALSE    # interval ouvert à droite
+)
+
+# ajoute un axe sur le coté droit
+axis(side = 4,
+     at = pretty(hist_data$counts),
+     labels = round(pretty(hist_data$counts) / length(revenus), 2))
+
+
+# polygone de fréquence
+midpoints <- hist_data$mids
+frequences <- hist_data$counts
+
+plot(midpoints,
+     frequences, 
+     type = "o",
+     xlab = "Revenus", 
+     ylab = "Fréquence", 
+     main = "Polygone de fréquences", 
+     # esthétique
+     lwd = 2, 
+     col = "black", 
+     pch = 16
+)
+
+axis(side = 4,
+     at = pretty(hist_data$counts),
+     labels = round(pretty(hist_data$counts) / length(revenus), 2))
+
+
+# données de la distribution 
+print(hist_data$density)
+print(hist_data$counts)
+print(sum(hist_data$counts))
+
+################################################################################
 # numero 4
 
 dataGain<-c(390,385,418,368,341,427,471,401,519,467,
@@ -16,7 +71,7 @@ dataGain<-c(390,385,418,368,341,427,471,401,519,467,
 sequence_intervalles <- seq(310, max(dataGain) + 30, by = 30)
 hist_data <- hist(dataGain,
                   breaks = sequence_intervalles,
-                  main = "Histograme du salaire", 
+                  main = "Histogramme du salaire", 
                   xlab = "Tranche", 
                   ylab = "Effectif",
                   col = "pink"
@@ -108,15 +163,12 @@ plot(centre_interv,
      type = "o",
      col = "blue",
      pch = 16,
-     lty = 1,
      xlab = "Revenus ", 
      ylab = "Fréquence",
      main = "Polygone des fréquences des revenus",
-     ylim = c(0, 0.5), # Ajuster les limites de l'axe Y
-     cex.lab = 1.5, 
-     cex.axis = 1.5, 
-     cex.main = 2
+     ylim = c(0, 0.5),          # Ajuster les limites de l'axe Y
 )
+
 # ajoute sur plot la ligne pour les femmes 
 # ATTENTION utiliser plot 2 fois va juste créer deux graphiqeus différents
 lines(centre_interv, frequence_femme, type = "o", col = "orange", pch = 16, lty = 2)
@@ -128,7 +180,7 @@ legend("topright",
        pch = 16
 )
 ################################################################################
-# numero 
+# numero 14
 
 # données
 ages <- c("15-19", "20-24", "25-29", "30-34", "35-39", "40-44", "45-49", "50-54", "55-59", "60-64")
@@ -163,11 +215,10 @@ lines(1:length(ages), frequence_veuves, type = "o", col = "red")
 lines(1:length(ages), frequence_divorcees, type = "o", col = "green")
 
 # axe avec l'age aux x
-axis(1, at = 1:length(ages), labels = ages)
+axis(side = 1, at = 1:length(ages), labels = ages)
 
 legend("topright", 
        legend = c("Célibataires", "Veuves", "Divorcées"),
        col = c("blue", "red", "green"), 
        lty = 1, 
        pch = 1)
-
